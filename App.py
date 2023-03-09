3ced1364e0167cdc2a67a22a56ff0e20336c81a5
Robin_Ponson <83652394+Rocksorr@users.noreply.github.com>
2023-03-09 18:00:12 +0100

    Add files via upload

---
+++ App.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Application de reconnaissance faciale et vocale")
+
+st.write("Cette application a été réalisée par : Laura Boutonnet, Nawres Dhiflaoui, Chrystelle Grosso et Robin Ponson")
+
+st.write("Elle a été réalisée dans le cadre de challenge de 47 heures de Webmining 2022")
+
+st.write("")
+
+st.write("Elle permet de détecter les visages présents dans une vidéo et de les identifier. Les informations concernant l'âge, le genre et l'émotiion sont également affichées.")
+
+st.write("")
+
+st.write("Elle présente deux onglets : un onglet accueil, un onglet reconnaissance faciale + vocale")
